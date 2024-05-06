6c69d9956e9b9fb0abdd870d697a7cb31f3e5bc9
asmondnoahloo <157396430+asmondnoahloo@users.noreply.github.com>
2024-05-06 18:20:43 +0800

    first commit

---
+++ starome/src/main/java/starome/backend/starome/StaromeApplication.java
@@ -0,0 +1,13 @@
+package starome.backend.starome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaromeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaromeApplication.class, args);
+	}
+
+}
---
+++ starome/src/test/java/starome/backend/starome/StaromeApplicationTests.java
@@ -0,0 +1,13 @@
+package starome.backend.starome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaromeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
